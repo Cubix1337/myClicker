8d7e93c9d4d82aeb275f86d35e4e4fabee78bc4b
Cubix1337 <antonio.roig123@gmail.com>
2018-12-12 00:25:28 +0000

    adding revision

--- main.js
+++ main.js
@@ -1,34 +1,45 @@
 //vars
-
 psc1 = document.getElementById("psc1")
 psc2 = document.getElementById("psc2")
 psc3 = document.getElementById("psc3")
 psc4 = document.getElementById("psc4")
 psc5 = document.getElementById("psc5")
-psc6 = document.getElementById("psc6")
-
-pscs = [psc1,psc2,psc3,psc4,psc5,psc6]
+pscs = [psc1,psc2,psc3,psc4,psc5]
 
 cardIndex = 0;
+
+//bonuses
+var bonuses= [
+/*0*/  {hp: 5},
+/*1*/  {hp: 10},
+/*2*/  {attack: 5},
+/*3*/  {attack: 2}
+]
+//skills
+var skills= [
+/*0*/{name:"Bash",image: "bash.gif",bonuses:[bonuses[0],bonuses[2]], cooldown:2},
+]
 //classes
 var swordsman ={
   name:"Swordsman",
   bonuses:[1,2],
   skills:[1,2],
-  availableClasses:[novice,swordsman]
+  availableClasses:[novice,swordsman],
+  image: "images/swordm.gif"
 }
 
 var novice ={
   name:"Novice",
   bonuses:[1,2],
-  skills:[1,2],
-  availableClasses:[swordsman]
+  skills:[skills[0]],
+  availableClasses:[swordsman],
+  image: "images/novicem.jpg"
 }
 
-function Card (id,name,source,summoned,attack,hp,defence,bounty,bonuses,instance,type){
+function Card (id,name,image,summoned,attack,hp,defence,bounty,bonuses,instance,type){
   this.id = id
   this.name = name
-  this.source = source
+  this.image = image
   this.summoned = summoned
   this.attack = attack
   this.hp = hp
@@ -41,44 +52,14 @@ function Card (id,name,source,summoned,attack,hp,defence,bounty,bonuses,instance
 
 //cards
   //mobs
-var poring = {
-  name:"Poring",
-  source:"poring.jpg",
-  summoned:"poring.gif",
-  attack: 1,
-  hp: 3,
-  defence:0,
-  bounty: 100,
-  bonuses:[1,2],
-  instance:0,
-  type:"mob"
-}
+var poring = {name:"Poring",image:"poring.jpg",summoned:"poring.gif",attack: 1,hp: 3,defence:0,bounty: 100,bonuses:[1,2],instance:0,type:"mob"}
+var picky = {name:"Picky",image:"picky.jpg",summoned:"picky.gif",attack: 2,hp: 5,defence:0,bounty: 100,instance:0,type:"mob"}
 
-var picky = {
-  name:"Picky",
-  source:"picky.jpg",
-  summoned:"picky.gif",
-  attack: 2,
-  hp: 5,
-  defence:0,
-  bounty: 100,
-  instance:0,
-  type:"mob"
-}
-
-//bonuses
-var bonuses= [
-/*0*/  {hp: +5},
-/*1*/  {hp: +10},
-/*2*/  {attack: +5}
-]
-
-//skill cards
 //equips
 //consumables
 var redPotion = {
 name:"Red Potion",
-source: "redpot.jpg",
+image: "redpot.jpg",
 type: "consumable",
 bonuses:[bonuses[0],bonuses[2]],
 instance:0
@@ -100,7 +81,7 @@ var inPlayPlayer=[];
 
 function objClone(original){
 let newID = original.name.toLowerCase() + original.instance
-activePlayer.hand.push(new Card(newID, original.name, original.source, original.summoned, original.attack, original.hp,original.defence,original.bounty,original.bonuses,original.instance, original.type))
+activePlayer.hand.push(new Card(newID, original.name, original.image, original.summoned, original.attack, original.hp,original.defence,original.bounty,original.bonuses,original.instance, original.type))
 }
 
 var enemy = {
@@ -142,26 +123,22 @@ console.log(drawnCard)
 
 function cardRender(card){
   image = document.createElement("IMG");
-  source = document.createAttribute("src");source.value = "images/"+ card.source;
+  source = document.createAttribute("src");source.value = "images/"+ card.image;
   image.setAttributeNode(source);image.setAttribute("id", card.name.toLowerCase()+(card.instance+1));image.setAttribute("draggable",true);image.setAttribute("ondragstart","drag(event)");
   target = document.getElementById("player-hand");target.appendChild(image);
   card.instance++;
 }
 
 function pve(activePlayer,card){
-  //make modal canvas later
-  modal.style.display = "block";
-  let patck = document.getElementById("player-attack");
-  let php = document.getElementById("player-hp")
-  patck.innerHTML = activePlayer.attack;
-  php.innerHTML = activePlayer.hp;
-  let eatck = document.getElementById("enemy-attack");
-  let ehp = document.getElementById("enemy-hp");
+//make modal canvas later
+updateHP(card);
+modal.style.display="block";
+  setTimeout(function(){
   let esprite = document.getElementById("enemy-sprite");
   esprite.children[0].src="images/"+card.summoned;
-  eatck.innerHTML = card.attack;
-  ehp.innerHTML = card.hp;
-}
+  let psprite = document.getElementById("player-sprite").children[0];
+psprite.classList.remove('attackanim');esprite.children[0].classList.remove('attackanim');esprite.children[0].classList.remove('deathanim')
+},800)}
 
 var cardOrder = [];
 var cardEvalIndex = 0;
@@ -184,7 +161,7 @@ for (var i = 0; i < activePlayer.hand.length; i++) {
 function evaluator(){
 if (cardOrder.length > cardEvalIndex){
 return cardOrder[cardEvalIndex].type
-}else{console.log("no")}
+}else{console.log("no");modal.style.display="none";}
 }
 
 function pveCaller(activePlayer){
@@ -198,7 +175,7 @@ switch(evaluator(activePlayer)) {
       console.log("mvp")
       break;
   case "skill":
-      console.log("skill")
+      skillUse(cardOrder[cardEvalIndex])
   case "equipment":
       console.log("equipment")
   case "consumable":
@@ -210,31 +187,121 @@ switch(evaluator(activePlayer)) {
 
 function timerRunner(activePlayer,card){
 pve(activePlayer,card);
+let psprite = document.getElementById("player-sprite").children[0];
+let esprite = document.getElementById("enemy-sprite").children[0];
+psprite.classList.remove('attackanim');
+esprite.classList.remove('attackanim');
 timerInt = window.setInterval(function (){
-if (card.hp !=0 && card.hp >0 && activePlayer.hp > 0) {card.hp = card.hp - activePlayer.attack;console.log("I ran");pve(activePlayer,card);
-if (card.hp !=0 && card.hp >0 && activePlayer.hp > 0) {activePlayer.hp = activePlayer.hp - card.attack;pve(activePlayer,card);
-}} else if (activePlayer.hp <= 0){console.log("gameOver");clearInterval(timerInt);}else{card.hp = 0;console.log("card is dead");cardEvalIndex++;clearInterval(timerInt);console.log("I cleared");modal.style.display="none";pveCaller(activePlayer)}
-},1500)}
+if (card.hp !=0 && card.hp >0 && activePlayer.hp > 0) {card.hp = card.hp - activePlayer.attack;psprite.classList.add('attackanim');pve(activePlayer,card)}
+if (card.hp !=0 && card.hp >0 && activePlayer.hp > 0) {activePlayer.hp = activePlayer.hp - card.attack;esprite.classList.add('attackanim');pve(activePlayer,card)}
+if (activePlayer.hp <= 0){console.log("gameOver");clearInterval(timerInt)};
+if (card.hp<=0){card.hp = 0;updateHP(card);esprite.classList.add('deathanim');cardEvalIndex++;clearInterval(timerInt);pveCaller(activePlayer)}
+},2000)}
 
-
-function skillUse(card){
+function skillUse(skill){
+  for (var i = 0; i < card.bonuses.length; i++) {
+  'hp' in card.bonuses[i]?activePlayer.hp+=card.bonuses[i].hp:console.log("failed to find hp key in obj");
+  'attack' in card.bonuses[i]?activePlayer.attack *= card.bonuses[i].attack:console.log("failed to find attack key in obj")
+  }
+cardEvalIndex++;
 }
 function consume(card){
   for (var i = 0; i < card.bonuses.length; i++) {
   'hp' in card.bonuses[i]?activePlayer.hp+=card.bonuses[i].hp:console.log("failed to find hp key in obj");
-  'attack' in card.bonuses[i]?activePlayer.hp+=card.bonuses[i].hp:console.log("failed to find attack key in obj")
+  'attack' in card.bonuses[i]?activePlayer.attack+=card.bonuses[i].attack:console.log("failed to find attack key in obj")
   }
 cardEvalIndex++;
 pveCaller(activePlayer)
 }
 
-//switch could use iteration as arg or helper function to iterate(ineficient)
-// switch (value in card.bonuses[0]) {
-//   case expression:
-//
-//     break;
-//   default:
+function updateHP(card){
+let patck = document.getElementById("player-attack");let php = document.getElementById("player-hp");patck.innerHTML = activePlayer.attack;php.innerHTML = activePlayer.hp;let eatck = document.getElementById("enemy-attack");let ehp = document.getElementById("enemy-hp");eatck.innerHTML = card.attack;ehp.innerHTML = card.hp;
+}
 
 function equip(card){
 
+}
+
+function playerHeaderPopulator(){
+let phead = document.getElementById("player-header");
+phead.innerHTML="<img src='"+player.class.image+"'><p>Name: "+player.name+"</p><p>HP: "+player.hp+"</p><p>Class: "+player.class.name+"</p><p>Zeny: "
+phead.innerHTML+="<div id='skillBar'><img src='images/"+activePlayer.class.skills[0].image+"'><img src='images/"+activePlayer.class.skills[0].image+"'><div>"
+}
+playerHeaderPopulator()
+//phaser
+// var config = {
+//        type: Phaser.AUTO,
+//        width: 850,
+//        height: 250,
+//        backgroundColor: '#ffffff',
+//        scene: {
+//            preload: preload,
+//            create: create,
+//            update: update
+//        },
+//        parent:'divider'
+//    };
+//
+//    var game = new Phaser.Game(config);var anim;var sprite;var progress;var frameView;
+//
+//    function preload ()
+//    {
+//      this.load.spritesheet('drops', 'images/drops.png', { frameWidth: 41, frameHeight: 39 });
+//      this.load.image('novicem','images/novicem.gif')
+//    }
+//    function create ()
+//    {
+//      var config = {
+//        key: 'static',
+//        frames: this.anims.generateFrameNumbers('drops'),
+//        frameRate: 10,
+//        repeat: -1
+//    };
+//    anim = this.anims.create(config);
+//    console.log(anim);
+//    sprite = this.add.sprite(250, 125, 'drops').setScale(1);
+//    console.log(sprite);
+//    sprite.anims.load('static');
+//    sprite.anims.play('static');
+//    this.add.image(450, 125, 'novicem').setOrigin(0, 0);
+//    }
+//
+//    function update ()
+//    {}
+
+
+function timerCall(card){
+  pve(activePlayer,card);
+timerInit(card)
+}
+
+function timerInit(card){
+  modal.style.display="block"
+  let psprite = document.getElementById("player-sprite").children[0];
+  let esprite = document.getElementById("enemy-sprite").children[0];
+newTimer = window.setInterval(function (){
+  setTimeout(function () {psprite.classList.remove('attackanim')},500)
+  setTimeout(function () {esprite.classList.remove('attackanim')},500)
+  console.log("sprites had class removed")
+switch (card.hp > 0) {
+  case card.hp !=0 && card.hp >0 && activePlayer.hp > 0:
+    card.hp-activePlayer.attack <=0 ? card.hp = 0:card.hp-=activePlayer.attack;updateHP(card);psprite.classList.add('attackanim')
+    if (card.hp >0){
+      activePlayer.hp = activePlayer.hp - card.attack;
+      updateHP(card);esprite.classList.add('attackanim')
+      activePlayer.hp -=card.attack;
+      console.log(activePlayer.hp);
+    } else{
+      card.hp=0;console.log("card dead");esprite.style.opacity=0;
+      setTimeout(function () {clearInterval(newTimer);console.log("timer is stopped")}, 1000);
+    }
+    break;
+  default:{}
+}},3000)
+}
+
+
+function checker(v){
+let x = Math.floor(Math.random() *100);
+x>v? console.log(x+" procced"):console.log("didnt")
 }
